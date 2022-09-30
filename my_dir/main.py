df610ad2b7226a3ebab56e24977f5ab1f60d27de
Nicholas Lu <114760280+NicholasLu1@users.noreply.github.com>
2022-09-30 16:28:08 +0000

    Last change: We stop the method from unnecessarily looping through parts of the list that don't need to be sorted. Thus if the method does not find anything that's unsorted, it will skip to the next stage, increasing efficiency.

--- my_dir/main.py
+++ my_dir/main.py
@@ -3,10 +3,14 @@ import random
 def bubble_sort(in_list):
   for stage_num in range(len(in_list) - 1):
     print(f"Stage {stage_num + 1}")
+    found_unsorted = False
     for i in range(len(in_list) - 1 - stage_num):
       if in_list[i] > in_list[i + 1]:
+        found_unsorted = True
         print(f"Moving index {i} to index {i+1}")
         in_list[i], in_list[i + 1] = in_list[i + 1], in_list[i]
+    if not found_unsorted:
+      break
 
 sample_list = list(range(10))
 sample_list = list(range(100, 110)) 
