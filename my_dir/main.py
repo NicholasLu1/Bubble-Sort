4ad5a95383696a46eb8a5bb3a48031f98b462f6c
Nicholas Lu <114760280+NicholasLu1@users.noreply.github.com>
2022-09-30 16:25:06 +0000

    Later stages decease method efficiency, thus we will ignore them.

--- my_dir/main.py
+++ my_dir/main.py
@@ -3,7 +3,7 @@ import random
 def bubble_sort(in_list):
   for stage_num in range(len(in_list) - 1):
     print(f"Stage {stage_num + 1}")
-    for i in range(len(in_list) - 1):
+    for i in range(len(in_list) - 1 - stage_num):
       if in_list[i] > in_list[i + 1]:
         print(f"Moving index {i} to index {i+1}")
         in_list[i], in_list[i + 1] = in_list[i + 1], in_list[i]
@@ -16,3 +16,4 @@ random.shuffle(sample_list)
 
 print(sample_list)
 bubble_sort(sample_list)
+print("Sorted: ", sample_list)
