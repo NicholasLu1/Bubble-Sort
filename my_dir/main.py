23939cbf23e4e853b40432488e031f7b4d6ff725
Nicholas Lu <114760280+NicholasLu1@users.noreply.github.com>
2022-09-30 16:15:24 +0000

    Expanded function to assign a stage to every number.

--- my_dir/main.py
+++ my_dir/main.py
@@ -1,18 +1,17 @@
 import random
 
 def bubble_sort(in_list):
-  for stage in ["Stage 1", "Stage 2", "Stage 3"]:
-    print(stage)
+  for stage_num in range(len(in_list)):
+    print(f"Stage {stage_num + 1}")
     for i in range(len(in_list) - 1):
       if in_list[i] > in_list[i + 1]:
-        print(f"The list is {in_list}")
-        print(f"Moving {in_list[i]}") 
+        print(f"Moving index {i} to index {i+1}")
         in_list[i], in_list[i + 1] = in_list[i + 1], in_list[i]
 
 sample_list = list(range(10))
-sample_list.insert(random.randrange(10), 100)
-sample_list.insert(random.randrange(10), 101)
-sample_list.insert(random.randrange(10), 102)
+sample_list = list(range(100, 110)) 
+random.shuffle(sample_list)
+
 
 
 print(sample_list)
